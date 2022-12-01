a58a0f0854fe8b7d1044fdab57db843e6f5edda4
3JuIkA <rediskapopov27@gmail.com>
2022-12-01 20:29:04 +0200

    Added part4

---
+++ Part4/Program.cs
@@ -0,0 +1,21 @@
+﻿int a = 1;
+bool not = true;
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Чётные числа от 1 до " + num);
+while (a <= num)
+{
+    if (a % 2 != 1)
+    {
+        Console.WriteLine(a + ", ");
+        not = false;
+    }
+    a++;
+}
+
+if (not)
+{
+    Console.WriteLine("Нет чётных чисел");
+}
